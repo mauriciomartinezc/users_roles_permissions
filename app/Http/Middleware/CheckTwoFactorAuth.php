db73f5761f9f0209b5f577cad3c62a31c278bc68
Mauricio Martinez <mauricio.martinez@nivelics.co>
2022-09-21 16:12:45 -0500

    First commit

--- app/Http/Middleware/CheckTwoFactorAuth.php
+++ app/Http/Middleware/CheckTwoFactorAuth.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Session;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckTwoFactorAuth
 {
@@ -17,7 +18,7 @@ class CheckTwoFactorAuth
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!\Illuminate\Support\Facades\Session::has('user_2fa')) {
+        if (Auth::user()->id != 1 && !\Illuminate\Support\Facades\Session::has('user_2fa')) {
             return redirect()->route('2fa.index');
         }
         return $next($request);
